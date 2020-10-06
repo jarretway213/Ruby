9d6a26813be170ed015b42867f9ce9d75f8dd698
Jarret Way <jway2@colum.edu>
2020-10-05 22:22:15 -0500

    Third Commit
    Third Commit

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,14 +7,20 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
+    /// Update is a method used to change the position of our character. It uses the following lines of code to execute this task.
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
-        transform.position = position;
+	
+	float horizontal = Input.GetAxis("Horizontal");
+	float vertical = Input.GetAxis("Vertical");
+	//We have just created two new variables relating to the horizontal and vertical axes for Ruby to move on.
+	    
+		Vector2 position = transform.position;
+		position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+		position.y = position.y + 3.0f * vertical * Time.deltaTime;
+		transform.position = position;
     }
 }
